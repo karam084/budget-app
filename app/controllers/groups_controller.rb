eea6eef7936dec210facfe0c0497eb3ac359f385
karam084 <karam.ezz84@gmail.com>
2022-12-02 01:37:42 +0200

    Add users and group of entity

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,21 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_group, only: %i[show edit update destroy]
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups
+    @users = current_user.name
   end
 
   # GET /groups/1 or /groups/1.json
-  def show; end
+  def show
+    @users = current_user.name
+  end
 
   # GET /groups/new
   def new
-    @group = Group.new
+    @group = current_user.groups.build
   end
 
   # GET /groups/1/edit
@@ -19,8 +23,7 @@ class GroupsController < ApplicationController
 
   # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
-
+    @group = current_user.groups.build(group_params)
     respond_to do |format|
       if @group.save
         format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
