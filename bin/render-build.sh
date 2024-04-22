37f17adb50bf30f487016e1bc6fce7cc4cf9312e
karam084 <karam.ezz84@gmail.com>
2024-04-22 02:55:52 +0200

    Add render file

---
+++ bin/render-build.sh
@@ -0,0 +1,7 @@
+#!/user/bin/env bash# exit on error                                             
+set -o errexit
+
+
+bundle install
+bundle exec rails assests:precompile
+bundle exec rails assets:clean
